0fe396a74706fa5f1aa79581d21e4e65480a66a0
Yar <yarsher.dev@gmail.com>
2019-11-12 23:42:30 -0500

    b1

--- app/src/main/java/animation/xyz/animation/MainActivity.java
+++ app/src/main/java/animation/xyz/animation/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         b=(Button)findViewById(R.id.android1);
 
         b.setOnClickListener(new View.OnClickListener() {
