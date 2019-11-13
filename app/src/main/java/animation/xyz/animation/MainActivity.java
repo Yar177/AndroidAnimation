ce20775411e927787b26dcb6679c1774c80c5f33
Yar <yarsher.dev@gmail.com>
2019-11-12 23:43:15 -0500

    github0

--- app/src/main/java/animation/xyz/animation/MainActivity.java
+++ app/src/main/java/animation/xyz/animation/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Animation animation=AnimationUtils.loadAnimation(MainActivity.this,R.anim.rotate);
                 b.startAnimation(animation);
-
             }
         });
 
